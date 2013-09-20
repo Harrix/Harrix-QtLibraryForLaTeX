602870ef674c9e86afd3735e9a7d506845916f9e
Harrix <sergienkoanton@mail.ru>
2013-09-21 03:17:49 +0800

    Наброски

---
+++ HarrixQtLibraryForLaTeX.cpp
@@ -0,0 +1,7 @@
+//HarrixQtLibraryForLaTeX
+//Версия 1.0
+//Библиотека для отображения различных данных в LaTeX файлах.
+//https://github.com/Harrix/HarrixQtLibraryForLaTeX
+//Библиотека распространяется по лицензии Apache License, Version 2.0.
+
+#include "HarrixQtLibraryForLaTeX.h"
